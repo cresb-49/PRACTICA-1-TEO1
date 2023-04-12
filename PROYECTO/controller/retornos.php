195d72f81cde5d6e124f6ce277e966c5bae28579
cresb-49 <carlosbpac@gmail.com>
2023-04-12 01:30:30 -0600

    formato de codigo

--- PROYECTO/controller/retornos.php
+++ PROYECTO/controller/retornos.php
@@ -8,22 +8,23 @@ class Retornos
         }
     }
 
-    static function returnIndexIfNotLogin()
+    public static function returnIndexIfNotLogin()
     {
         if (!(isset($_SESSION['username']))) {
             header('Location: http://localhost/PRACTICA-1-TEO1/PROYECTO/index.php');
         }
     }
 
-    public static function returnIndexIfAdmin(){
-         if (isset($_SESSION['rol'])) {
+    public static function returnIndexIfAdmin()
+    {
+        if (isset($_SESSION['rol'])) {
             if ($_SESSION['rol'] === 'ADMIN') {
                 header('Location: http://localhost/PRACTICA-1-TEO1/PROYECTO/index.php');
             }
         } else {
             header('Location: http://localhost/PRACTICA-1-TEO1/PROYECTO/index.php');
         }
-    }   
+    }
 
     public static function returnIndexIfNotAdmin()
     {
